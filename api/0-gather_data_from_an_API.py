111ab4895b5a3a8edc8ced9aaa992ddf8f5f35b1
Melissa Arroyo <melissaarroyotorres9806@gmail.com>
2023-04-10 18:49:01 -0400

    0-gather_data_from_an_API.py

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Documentation"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    employee_id = int(sys.argv[1])
+
+    api_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
+    response = requests.get(api_url)
+
+    employee_name = response.json()["name"]
+
+    api_url2 = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
+    response = requests.get(api_url2)
+
+    tasks = response.json()
+    total_tasks = len(tasks)
+
+    completed_tasks = []
+    for task in tasks:
+        if task["completed"]:
+            completed_tasks.append(task)
+
+    n_total_tasks = len(completed_tasks)
+
+    print(f"Employee {employee_name} is done with tasks({n_total_tasks}/{total_tasks}):")
+
+for task in completed_tasks:
+    print(f"\t {task['title']}")
