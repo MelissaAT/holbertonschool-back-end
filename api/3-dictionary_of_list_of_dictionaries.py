c842065fe8e8cf962f8a6ebb0af0fa75cefe13e8
Melissa Arroyo <melissaarroyotorres9806@gmail.com>
2023-04-10 20:28:21 -0400

    3-dictionary_of_list_of_dictionaries.py

---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Documentation"""
+import json
+import requests
+
+api_users_url = 'https://jsonplaceholder.typicode.com/users'
+api_todos_url = 'https://jsonplaceholder.typicode.com/todos'
+
+response = requests.get(api_users_url)
+users = response.json()
+
+user_ids = []
+for user in users:
+    user_ids.append(user["id"])
+
+json_dict = {}
+
+for employee_id in user_ids:
+    api_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
+    response = requests.get(api_url)
+
+    employee_name = response.json()["username"]
+
+    api_url2 = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
+    response = requests.get(api_url2)
+
+    tasks = response.json()
+
+    json_dict[employee_id] = []
+    for task in tasks:
+        json_format = {
+            "username": employee_name,
+            "task": task["title"],
+            "completed": task["completed"]
+        }
+        json_dict[employee_id].append(json_format)
+
+with open("todo_all_employees.json", 'w') as json_file:
+    json.dump(json_dict, json_file)
+
+
